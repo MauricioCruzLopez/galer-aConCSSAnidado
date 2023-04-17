d86fde023aa6e5a5e64959a9857517399f073202
Mauricio Cruz <mauriciogonzalolopezcruz@gmail.com>
2023-04-17 19:35:09 -0300

    el código de javascript se hizo para comprobar la lógica para una sola imagen

--- javascript/davueltalastarjetas.js
+++ javascript/davueltalastarjetas.js
@@ -1,5 +1,24 @@
-const art1 = document.getElementById("art1");
+const imagen1 = document.getElementById("imagen1");
+const art1eliminado = imagen1;
 
-art1.addEventListener("click",()=>{
-    alert("funciona");
+const autor1 = document.getElementById("autor1");
+const autor1eliminado = autor1;
+
+const carta1 = document.getElementById("carta1");
+
+imagen1.addEventListener("click",()=>{ //da vuelta la carta para que se vea el autor
+    imagen1.remove();
+    autor1.classList.toggle("div-desactivado"); 
+    autor1.classList.toggle("div-activado");
+    if(autor1.parentElement === null){
+        carta1.appendChild(autor1eliminado);
+    }
 });
+autor1.addEventListener("click", ()=>{ //da vuelta la carta para que se vea la imagen
+    autor1.remove();
+    carta1.appendChild(art1eliminado);
+});     
+
+
+
+
